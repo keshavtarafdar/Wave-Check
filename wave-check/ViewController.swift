7bab2d36c73d342617868f375484389bd3a81410
keshavtarafdar <keshavtarafdar@gmail.com>
2021-12-12 21:15:40 -0500

    Initial Commit

---
+++ wave-check/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wave-check
+//
+//  Created by Keshav Tarafdar on 12/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
