2e20a8f6d704a5a3977be7317f607e2823b50d40
keshavtarafdar <keshavtarafdar@gmail.com>
2022-01-10 09:51:58 -0500

    Rounded buttons :)

--- wave-check/ViewController.swift
+++ wave-check/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // to get this line of code, open controller scene on one side, and view controller on other side
+    // open all stack views, and then click on specific button
+    // hold control and then click and drag into code, and give variable a name
+    @IBOutlet weak var viewMapButton: UIButton!
+    @IBOutlet weak var contributeDataButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // code to round button corners
+        viewMapButton.layer.cornerRadius = 22
+        contributeDataButton.layer.cornerRadius = 22
+        
     }
 
 
